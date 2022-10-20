315d06375dc46f845f1ce86e5c4329783e91da89
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2022-10-20 23:41:08 +0300

    Change name of properties

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 import { window, workspace } from "vscode";
 
 export function getPathToScripts() : string | null {
-    return workspace.getConfiguration("", workspace.workspaceFile).get("Directories.PathToScripts");
+    return workspace.getConfiguration("", workspace.workspaceFile).get("LTX.Directories.PathToScripts");
 }
 
 export function isIgnoreParamsDiagnostic() : boolean {
-    return workspace.getConfiguration("", workspace.workspaceFile).get("Diagnostics.IgnoreParamsDiagnostic")
+    return workspace.getConfiguration("", workspace.workspaceFile).get("LTX.Diagnostics.IgnoreParamsDiagnostic")
 }
 
 export function isDiagnosticEnabled() : boolean {
