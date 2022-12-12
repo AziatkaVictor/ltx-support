6e0a4707deeea190734ec11facb1ea6621974cd5
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2022-12-12 20:18:23 +0100

    Refactor code for LtxCondlist

--- src/ltx/ltxCondlist.ts
+++ src/ltx/ltxCondlist.ts
@@ -10,6 +10,10 @@ export class LtxCondlist {
     readonly functionRange?: Range | null;
     readonly sectionLink?: LtxSectionLink;
 
+    private replaceText(data, text) {
+        return data.replace(new RegExp("(?<=\\b)\\" + text + "(?=\\b)"), " ".repeat(text.length));
+    }
+
     constructor(lineNumber: number, index: number, data: string) {
         let tempData = data;
 
@@ -17,10 +21,10 @@ export class LtxCondlist {
         this.function = /\%.*?\%/.exec(tempData);
 
         if (this.condition) {
-            this.conditionRange = new Range(new Position(lineNumber, index + this.condition.index), new Position(lineNumber, index + this.condition[0].length + this.condition.index));
+            this.conditionRange = new Range(new Position(lineNumber, index + this.condition.index), new Position(lineNumber, index + this.condition[0].length + this.condition.index - 1));
         }
         if (this.function) {
-            this.functionRange = new Range(new Position(lineNumber, index + this.function.index), new Position(lineNumber, index + this.function[0].length + this.function.index));
+            this.functionRange = new Range(new Position(lineNumber, index + this.function.index), new Position(lineNumber, index + this.function[0].length + this.function.index - 1));
         }
 
         let search = /(\+|\-)\w*\b(?<=\w)/g
@@ -28,28 +32,28 @@ export class LtxCondlist {
         while ((match = search.exec(tempData)) !== null) {
             let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
             addSemantic(new LtxSemantic(LtxSemanticType.variable, LtxSemanticModification.declaration, tempRange, LtxSemanticDescription.info, match[0]))
-            tempData = replaceText(tempData, match[0]);
+            tempData = this.replaceText(tempData, match[0]);
         }
 
         search = /(\=|\!)\w*\b(?<=\w)/g
         while ((match = search.exec(tempData)) !== null) {
             let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
             addSemantic(new LtxSemantic(LtxSemanticType.function, LtxSemanticModification.declaration, tempRange, LtxSemanticDescription.signal, match[0]))
-            tempData = replaceText(tempData, match[0]);
+            tempData = this.replaceText(tempData, match[0]);
         }
 
         search = new RegExp("\\b(nil|true|false|complete|fail)\\b", "g")
         while ((match = search.exec(tempData)) !== null) {
             let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
             addSemantic(new LtxSemantic(LtxSemanticType.keyword, LtxSemanticModification.readonly, tempRange, LtxSemanticDescription.signal, match[0]))
-            tempData = replaceText(tempData, match[0]);
+            tempData = this.replaceText(tempData, match[0]);
         }
 
         search = /(?<!\w)\d+/g
         while ((match = search.exec(tempData)) !== null) {
             let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
             addSemantic(new LtxSemantic(LtxSemanticType.number, null, tempRange, LtxSemanticDescription.signal, match[0]))
-            tempData = replaceText(tempData, match[0]);
+            tempData = this.replaceText(tempData, match[0]);
         }
 
         for (let count = 0; count < sectionsArray.length; count++) {
@@ -57,8 +61,8 @@ export class LtxCondlist {
             search = new RegExp("(?<=\\b)" + sectionName + "(?![\\w\\@]+)(?=\\b)", "g")
             while ((match = search.exec(tempData)) !== null) {
                 let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
-                addSemantic(new LtxSemantic(LtxSemanticType.class, LtxSemanticModification.definition, tempRange, LtxSemanticDescription.signal, match[0]))
-                tempData = replaceText(tempData, sectionName);
+                addSemantic(new LtxSemantic(LtxSemanticType.class, LtxSemanticModification.definition, tempRange, LtxSemanticDescription.sectionLink, match[0]))
+                tempData = this.replaceText(tempData, sectionName);
             }
         }
 
@@ -66,12 +70,7 @@ export class LtxCondlist {
         while ((match = search.exec(tempData)) !== null) {
             let tempRange = new Range(new Position(lineNumber, index + match.index), new Position(lineNumber, index + match.index + match[0].length))
             addSemantic(new LtxSemantic(LtxSemanticType.string, null, tempRange, LtxSemanticDescription.signal, match[0]))
-            tempData = replaceText(tempData, match[0]);
+            tempData = this.replaceText(tempData, match[0]);
         }
     }
-}
-
-function replaceText(data, text) {
-    let tempReplace = " ".repeat(text.length);
-    return data.replace(new RegExp("(?<=\\b)\\" + text + "(?=\\b)"), tempReplace);
 }
