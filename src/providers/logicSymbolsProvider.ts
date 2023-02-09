fd4e5a0a549b7d0db9ef8f3fa3b947d75e7c458e
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2023-02-09 21:36:33 +0100

    Fix lines symbols

--- src/providers/logicSymbolsProvider.ts
+++ src/providers/logicSymbolsProvider.ts
@@ -6,6 +6,9 @@ export function provideSymbols(document: TextDocument, token: CancellationToken)
     var lines = []
     var sections = data.getSections().map(section => {
         Array.from(section.lines.values()).map(line => {
+            if (!line.propertyName) {
+                return;
+            }
             lines.push(new SymbolInformation(line.propertyName, SymbolKind.Property, section.name, new Location(document.uri, new Range(new Position(line.index, 0), new Position(line.index, line.rawData.length - 1)))));
         })
         return new SymbolInformation(section.name, SymbolKind.Class, null, new Location(document.uri, new Range(new Position(section.startLine, 0), new Position(section.endLine + 1, 0))));
