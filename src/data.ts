acb0a720219af295bb612bb559ebb3fb19c22b54
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2023-02-09 20:42:47 +0100

    Remove data file

--- src/data.ts
+++
@@ -1,6 +0,0 @@
-/** Информация о секциях и их параметрах */
-export const SECTIONS_DATA = require("../data/sections_documentation.json").sections;
-/** Информация о параметрах, которые используются в секциях */
-export const PARAMS_DATA = require("../data/sections_documentation.json").params;
-/** Список параметров, которые есть в любой секции */
-export const CONDITIONS_DATA: string[] = require("../data/sections_documentation.json").basedConditions;
