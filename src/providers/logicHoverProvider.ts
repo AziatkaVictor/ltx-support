7093e22454533e2e655f879647f3a93c97440969
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2023-03-10 21:11:35 +0100

    Params Hover

--- src/providers/logicHoverProvider.ts
+++ src/providers/logicHoverProvider.ts
@@ -2,22 +2,18 @@ import { TextDocument, Position, CancellationToken, Hover, ProviderResult, Markd
 import { DocumentationKind, getDocumentation } from "../documentation";
 import { getLtxDocument } from "../extension";
 import { LtxSemanticType } from "../ltx/ltxSemantic";
-import { getParameterType } from "../lua/modulesParser";
 
 export function provideHover(document: TextDocument, position: Position, token?: CancellationToken): ProviderResult<Hover> {
     const fileLtx = getLtxDocument(document);
     const semantic = fileLtx.getSemanticByPostition(position);
-    var Mark : MarkdownString = new MarkdownString();
+    var Mark = new MarkdownString();        
+    Mark.supportHtml = true;
 
     if (semantic.type === LtxSemanticType.function) {
         Mark = getDocumentation(semantic.text.slice(1, semantic.text.length), DocumentationKind.Functions, true);
-        Mark.supportHtml = true;
-        return new Hover(Mark);
     }
     else if (semantic.type === LtxSemanticType.property) {
-        var sectionName = fileLtx.getSectionByPosition(position).type.name;
-        Mark = new MarkdownString(getParameterType(semantic.text, sectionName));
-        Mark.supportHtml = true;
-        return new Hover(Mark);
-    }
+        Mark = getDocumentation(semantic.text.replace(/[0-9]/g, ''), DocumentationKind.Property, true);
+    }        
+    return new Hover(Mark);
 }
