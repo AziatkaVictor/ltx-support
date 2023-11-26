0ceb6c009b6baeb802d5f310ea2662d03fc30040
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2023-11-26 11:51:16 +0100

    Unit tests base #34

---
+++ src/test/suite/index.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
+
+export function run(): Promise<void> {
+	// Create the mocha test
+	const mocha = new Mocha({
+		ui: 'tdd',
+        retries: 3,
+	});
+ 
+	const testsRoot = path.resolve(__dirname, '..');
+
+	return new Promise((success, error) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+			if (err) {
+				return error(err);
+			}
+
+			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				mocha.run(failures => {
+					if (failures > 0) {
+						error(new Error(`${failures} tests failed.`));
+					} else {
+						success();
+					}
+				});
+			} catch (err) {
+				console.error(err);
+				error(err);
+			}
+		});
+	});
+}
