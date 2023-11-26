49531b45cf0e0685170c6478db2631b0b97cb971
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2023-11-26 16:50:04 +0100

    Unit test passing for actions

--- src/test/suite/line.test.ts
+++ src/test/suite/line.test.ts
@@ -2,14 +2,14 @@ import * as assert from 'assert';
 
 suite('Line Test Suite', () => {
   test('Test 1', () => {
-    assert.strictEqual(2, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
   });
 
   test('Test 2', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   })
 
-  test('Test 3', (t) => {
+  test('Test 3', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
   })
 });
