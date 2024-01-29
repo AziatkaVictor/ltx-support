1dc76e1a2472e82c6b2129ffda1072ac920bb3aa
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2024-01-29 23:31:01 +0100

    Small changes in Section class

--- src/classes/sections/Common.ts
+++ src/classes/sections/Common.ts
@@ -5,15 +5,18 @@ import { Document } from "../documents/Index"
 export class Section {
     static namePattern = /(?<=\[).+(?=\])/g
     static bodyPattern = /^[\t\ ]*\[[^\r\n]*\](?:\:.+)?(?:[\r\n ]*(?:[^[\r\n ]+(?!\[))?)*$/gm
-
+    
     private declaration: Range
     private parameters: Parameter[]
+    
+    constructor(private owner: Document, private _range: Range) {}
+    
+    public get range(): Range {
+        return this._range;
+    }   
 
     public static getNamePatternWithParents(parents: string[]): RegExp {
         return new RegExp(Section.namePattern.source + "\:[^\n]*?\b(" + parents.join("|") + ")\b)", "g")
     }
-
-    constructor(private owner: Document, private range: Range) {
-
-    }
+    
 }
