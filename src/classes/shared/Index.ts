08fc5fe474373eb13b8723ef719b991ba4870bca
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2024-01-29 23:30:34 +0100

    Added DocumentsManager

--- src/classes/shared/Index.ts
+++ src/classes/shared/Index.ts
@@ -1,8 +1,10 @@
+import { DocumentsManager } from "./Manager"
 import { Parser } from "./Parser"
 import { Info } from "./Info"
 import { Condlist } from "./Condlist"
 
 export {
+    DocumentsManager,
     Parser,
     Info,
     Condlist
---
+++ src/classes/shared/Manager.ts
@@ -0,0 +1,25 @@
+import { TextDocument, Uri } from "vscode";
+import { Document } from "../documents/Index"
+import { DocumentFactory } from "../factories/Document";
+
+export class DocumentsManager {
+    private data: Map<Uri, Document>
+
+    constructor() {
+        this.data = new Map<Uri, Document>();
+    }
+
+    get(document: TextDocument): Document {
+        if (!this.data.has(document.uri)) {
+            this.set(document, DocumentFactory.create(document));
+        }
+        return this.data.get(document.uri);
+    }
+
+    set(document: TextDocument, value?: Document): void {
+        if (!value) {
+            value = DocumentFactory.create(document);
+        }
+        this.data.set(document.uri, value);
+    }
+}
