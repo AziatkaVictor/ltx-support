3383a8fd1213fe9ba923fa37927b27bdff7d0d4f
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2024-01-31 23:28:44 +0100

    Parameters for Sections

--- src/classes/sections/Common.ts
+++ src/classes/sections/Common.ts
@@ -1,11 +1,20 @@
 import { Range } from "vscode"
 import { Parameter } from "../parameters/Index"
 import { Document } from "../documents/Index"
+import { ParameterFactory, Parser } from "../ltx"
 
 export class Section {
     private declaration: Range
     private parameters: Parameter[]
     
+    constructor(private owner: Document, private _range: Range, readonly name: string = "Error") {
+        const lines = Parser.findAllRanges(this.owner.source, /^[^\r\n\[\]]+$/gm, this.range); 
+        if (lines) {
+            this.parameters = lines.map((value: Range, index: number, array: Range[]) => {
+                return ParameterFactory.create(this.owner, this, value);
+            });
+        }
+    }
 
     static get namePattern() {
         return new RegExp(/(?<=\[).+(?=\])/g);
@@ -17,6 +26,11 @@ export class Section {
     
     public get range(): Range {
         return this._range;
+
+    public get lastParameterLine(): number {
+        return Math.max(...this.parameters.map((value: Parameter, index: number, array: Parameter[]) => {
+            return value.range.start.line;
+        }));
     }   
 
     public static getNamePatternWithParents(parents: string[]): RegExp {
