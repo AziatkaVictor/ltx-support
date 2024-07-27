c8d5b35ce4cf1693c312b3a83ced3768e9ec6309
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2024-07-27 21:53:22 +0200

    Fix completion item provider

--- src/providers/CompletionItemProvider.ts
+++ src/providers/CompletionItemProvider.ts
@@ -4,9 +4,10 @@ import { DocumentsManager, Section } from "../classes/ltx";
 export class CustomCompletionProvider implements CompletionItemProvider {
     constructor(private data: DocumentsManager) {}
 
-    provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): ProviderResult<CompletionItem[] | CompletionList<CompletionItem>> {
+    async provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): Promise<CompletionItem[] | CompletionList<CompletionItem>> {
         const documentData = this.data.get(document);      
         
+        // Handle sections declaration
         if (context?.triggerCharacter == "[" || documentData.isSectionDeclaration(position)) {
             return [
                 new CompletionItem("sr_idle", CompletionItemKind.Class)
@@ -18,7 +19,7 @@ export class CustomCompletionProvider implements CompletionItemProvider {
         });
     }
 
-    resolveCompletionItem?(item: CompletionItem, token: CancellationToken): ProviderResult<CompletionItem> {
+    async resolveCompletionItem?(item: CompletionItem, token: CancellationToken): Promise<CompletionItem> {
         return item;
     }
 }
