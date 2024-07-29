cf6fb4244ac251f147a3259eb048aa488498bbf0
AziatkaVictor <85461004+AziatkaVictor@users.noreply.github.com>
2024-07-29 23:46:20 +0200

    DocumentFactory tests

--- src/test/suite/DocumentsFactory.test.ts
+++ src/test/suite/DocumentsFactory.test.ts
@@ -1,23 +1,29 @@
 import * as assert from 'assert';
 import { DocumentsFactory, DocumentsManager, LogicDocument } from '../../classes/ltx';
-import { Uri, workspace, window, TextEditor } from "vscode";
+import { Uri, workspace, window } from "vscode";
 import { TextEncoder } from 'util';
+import { before } from 'mocha';
 
 suite("Documents Factory", () => {
     suite('Logic', () => {
-        const content = '[logic@main]\nactive = nil';
-        const path = Uri.parse("./test/logic.ltx");
-        workspace.fs.writeFile(path, new TextEncoder().encode(content));
-        var editor = window.showTextDocument(path);
+        const path = Uri.parse("/test/logic.ltx");
+        before(async () => {
+            const content = '[logic@main]\nactive = nil';
+            await workspace.fs.writeFile(path, new TextEncoder().encode(content));
+            var editor = await window.showTextDocument(path);
+            await editor.document.save();
+        })
 
         test("File 'logic.ltx' exists", async () => {
             const manager = DocumentsManager.instance;
-            assert.equal(manager.has((await editor).document.uri), true);
+            const document = (await window.showTextDocument(path)).document;
+            assert.equal(manager.has(document.uri), true);
         });
 
         test("Type of 'logic.ltx' is logic", async () => {
-            const document = DocumentsFactory.instance.create((await editor).document);
-            assert.equal(document instanceof LogicDocument, true);
+            const document = (await window.showTextDocument(path)).document;
+            const createdDocument = DocumentsFactory.instance.create(document);
+            assert.equal(createdDocument instanceof LogicDocument, true);
         });
     });
 });
